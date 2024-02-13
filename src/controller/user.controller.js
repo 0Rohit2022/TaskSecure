a2445eb0d391472caa5053384c0490268ef988dd
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-02-13 18:02:51 +0530

    Database Connection established with production level file structure

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,15 @@
+import asyncHandler from "../utils/asyncHandler.js"
+import {User} from "../models/user.models.js"
+import ApiError from "../utils/ApiError.js"
 
-const getAllUser = () => {
 
-}
+const getAllUser = asyncHandler(async(req,res) => {
+  try {
+    // const user = await User.find()
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while getting all the user");
+  }
+})
 
 
 export {getAllUser}
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,7 +2,24 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-    
+    username : {
+        type: String, 
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim:true
+    },
+    email : {
+        type:String, 
+        required : true,
+        unique: true,
+        lowercase:true,
+        trim: true
+    },
+    password : {
+        type : String,
+        required :[true, "Password is required"]
+    }
 }, {timestamps: true});
 
 
