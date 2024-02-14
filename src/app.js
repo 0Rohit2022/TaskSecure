2e484c1d52147a2a6cab0853f2e49890b59c9d91
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-02-14 18:56:28 +0530

    cors origin updated

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const app = express();
 app.use(express.json({limit : "1mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 
 import userRouter from './routes/user.routes.js';
 import taskRouter from './routes/task.routes.js';
