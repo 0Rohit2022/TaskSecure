ef0a6550ad23feab376a3dd5f1d2963fd3f59efc
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-02-14 19:02:37 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
